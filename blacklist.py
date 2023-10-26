5630bce506f7f03cd87d1c53a362b21e265b4419
217heidai <217heidai@gmail.com>
2023-10-26 03:58:18 +0000

    过滤无效域名

--- blacklist.py
+++ blacklist.py
@@ -2,7 +2,9 @@ import os
 import sys
 import subprocess as sp
 from concurrent.futures import ThreadPoolExecutor,as_completed
+import asyncio
 
+import httpx
 from tcping import Ping
 
 from resolver import Resolver
@@ -13,6 +15,8 @@ class BlackList(object):
         self.__domainlistFile = os.getcwd() + "/rules/adblockdns.backup"
         self.__maxTask = 500
         self.__thread_pool = ThreadPoolExecutor(max_workers=self.__maxTask)
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"}
+        self.__client = httpx.AsyncClient(headers=headers)
 
     def GenerateDomainList(self):
         try:
@@ -52,6 +56,28 @@ class BlackList(object):
             print("%s.%s: %s" % (self.__class__.__name__, sys._getframe().f_code.co_name, e))
         finally:
             return blackList
+    
+    async def pingx(self, domain):
+        try:
+            url = "http://%s"%(domain)
+            response1 = await self.__client.get(url)
+            #print("1[%s]: %s"%(url, response1.status_code))
+        except Exception as e:
+            #print("2[%s]: %s"%(url, e))
+            response1 = None
+        try:
+            url = "https://%s"%(domain)
+            response2 = await self.__client.get(url)
+            #print("3[%s]: %s"%(url, response1.status_code))
+        except Exception as e:
+            #print("4[%s]: %s"%(url, e))
+            response2 = None
+        
+        if not response1 and not response2:
+            #print("5[%s]: Fail"%(domain))
+            return domain
+        else:
+            return None
 
     def GenerateBlackList(self, fileName, blackList):
         try:
@@ -70,6 +96,23 @@ class BlackList(object):
             total = len(domainList)
             if total < 1:
                 return
+            
+            # 启动异步循环
+            loop = asyncio.get_event_loop()
+            # 添加异步任务
+            taskList = []
+            for domain in domainList:
+                task = asyncio.ensure_future(self.pingx(domain))
+                taskList.append(task)
+            # 等待异步任务结束
+            loop.run_until_complete(asyncio.wait(taskList))
+            # 获取异步任务结果
+            blackList = []
+            for task in taskList:
+                domain = task.result()
+                if domain:
+                   blackList.append(domain) 
+            '''多线程
             count = (total + self.__maxTask - 1) // self.__maxTask
             taskList = []
             for i in range(self.__maxTask - 1):
@@ -85,6 +128,7 @@ class BlackList(object):
                 blackList += tmpList
             if len(blackList) < 1:
                 return
+            '''
             
             self.GenerateBlackList(self.__blacklistFile, blackList)
         except Exception as e:
