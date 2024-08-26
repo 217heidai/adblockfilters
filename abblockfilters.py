3c10cff5858a8bc9b173e3b4383eae603cda6c3f
217heidai <217heidai@gmail.com>
2024-08-26 05:44:17 +0000

    Star History Chart

--- abblockfilters.py
+++ abblockfilters.py
@@ -40,10 +40,12 @@ def CreatReadme(ruleList, fileName):
         f.write("# AdBlock DNS Filters\n")
         f.write("适用于AdGuard的去广告合并规则，每8个小时更新一次。\n")
         f.write("个人收藏了不少广告过滤规则，但是每次往新设备添加的时候很是头疼，于是写了这个项目，定时自动获取各规则源更新，生成合并规则库。\n")
+        f.write("\n")
         f.write("## 说明\n")
         f.write("1. 定时从上游各规则源获取更新，合并去重。\n")
         f.write("2. 使用两组国内、两组国外 DNS 服务，分别对上游各规则源拦截的域名进行解析，去除已无法解析的域名。（上游各规则源中存在大量已无法解析的域名，无需加入拦截规则）\n")
-        f.write("3. 本项目仅对上游规则进行合并、去重、去除无效域名，不做任何修改。如发现误拦截情况，可临时添加放行规则（如 `@@||www.example.com^$important`），并向上游规则反馈。\n\n")
+        f.write("3. 本项目仅对上游规则进行合并、去重、去除无效域名，不做任何修改。如发现误拦截情况，可临时添加放行规则（如 `@@||www.example.com^$important`），并向上游规则反馈。\n")
+        f.write("\n")
         f.write("## 订阅链接\n")
         f.write("1. AdGuard Home 等DNS拦截服务使用规则1\n")
         f.write("2. AdGuard 等浏览器插件使用规则1 + 规则2\n\n")
@@ -51,6 +53,7 @@ def CreatReadme(ruleList, fileName):
         f.write("|:-|:-|:-|\n")
         f.write("| 规则1：DNS 拦截 | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt) |\n")
         f.write("| 规则2：插件拦截 | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt) |\n")
+        f.write("\n")
         f.write("## 上游规则源\n")
         f.write("1. 感谢各位广告过滤规则维护大佬们的辛苦付出。\n")
         f.write("2. 不再引用[anti-AD](https://anti-ad.net/adguard.txt)、[yhosts](https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts.txt)，具体原因见[Mosney/anti-anti-AD](https://github.com/Mosney/anti-anti-AD)。\n")
@@ -61,6 +64,9 @@ def CreatReadme(ruleList, fileName):
         f.write("|:-|:-|:-|:-|:-|\n")
         for rule in ruleList:
             f.write("| %s | %s | [原始链接](%s) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/%s.txt) | %s |\n" % (rule[0],rule[1],rule[2],rule[0].replace(' ', '_'),rule[3]))
+        f.write("\n")
+        f.write("## Star History\n")
+        f.write("[![Star History Chart](https://api.star-history.com/svg?repos=217heidai/adblockfilters&type=Date)](https://star-history.com/#217heidai/adblockfilters&Date)\n")
 
 def GetBlackList():
     blackList = []
