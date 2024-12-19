c86669084dd02c4be8369c81b205e627c24c808a
217heidai <217heidai@gmail.com>
2024-12-19 10:45:05 +0800

    更换代理，增加jsdelivr主动刷新

--- readme.py
+++ readme.py
@@ -18,6 +18,11 @@ class ReadMe(object):
     def __init__(self, filename:str):
         self.filename = filename
         self.ruleList:List[Rule] = []
+        self.proxyList = [
+            "",
+            "https://gcore.jsdelivr.net/gh",
+            "https://github.boki.moe"
+        ]
 
     def getRules(self) -> List[Rule]:
         logger.info("resolve readme...")
@@ -27,11 +32,11 @@ class ReadMe(object):
                 line = line.replace('\r', '').replace('\n', '')
                 if line.find('|')==0 and line.rfind('|')==len(line)-1:
                     rule = list(map(lambda x: x.strip(), line[1:-1].split('|')))
-                    if rule[2].find('(') > 0 and rule[2].find(')') > 0 and len(rule) > 4:
+                    if rule[2].find('(') > 0 and rule[2].find(')') > 0 and rule[1].find('(') < 0:
                         url = rule[2][rule[2].find('(')+1:rule[2].find(')')]
                         matchObj1 = re.match('(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?', url)
                         if matchObj1:
-                            self.ruleList.append(Rule(rule[0], rule[1], url, rule[4]))
+                            self.ruleList.append(Rule(rule[0], rule[1], url, rule[-1]))
         return self.ruleList
 
     def getRulesNames(self) -> str:
@@ -44,6 +49,23 @@ class ReadMe(object):
 
     def setRules(self, ruleList:List[Rule]):
         self.ruleList = ruleList
+
+    def __subscribeLink(self, fileName:str, url:str=None):
+        link = ""
+
+        if url:
+            link += " [原始链接](%s) |"%(url)
+        else:
+            link += " [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/%s) |"%(fileName)
+        
+        for i in range(1, len(self.proxyList)):
+            proxy = self.proxyList[i]
+            if proxy.startswith("https://gcore.jsdelivr.net/"):
+                link += " [加速链接%d](%s/217heidai/adblockfilters@main/rules/%s) |"%(i, proxy, fileName)
+            else:
+                link += " [加速链接%d](%s/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/%s) |"%(i, proxy, fileName)
+        
+        return link
     
     def regenerate(self):
         logger.info("regenerate readme...")
@@ -55,27 +77,33 @@ class ReadMe(object):
             f.write("适用于AdGuard的去广告合并规则，每8个小时更新一次。\n")
             f.write("个人收藏了不少广告过滤规则，但是每次往新设备添加的时候很是头疼，于是写了这个项目，定时自动获取各规则源更新，生成合并规则库。\n")
             f.write("\n")
+
             f.write("## 说明\n")
             f.write("1. 定时从上游各规则源获取更新，合并去重。\n")
             f.write("2. 使用国内、国外各 3 组 DNS 服务，分别对上游各规则源拦截的域名进行解析，去除已无法解析的域名。（上游各规则源中存在大量已无法解析的域名，无需加入拦截规则）\n")
             f.write("3. 本项目仅对上游规则进行合并、去重、去除无效域名，不做任何修改。如发现误拦截情况，可临时添加放行规则（如 `@@||www.example.com^$important`），并向上游规则反馈。\n")
             f.write("\n")
-            f.write("## 订阅链接\n")
 
+            f.write("## 订阅链接\n")
             f.write("1. AdGuard Home 等 DNS 拦截服务使用规则1\n")
             f.write("2. AdGuard 等浏览器插件使用规则1 + 规则2\n")
             f.write("3. InviZible Pro、personalDNSfilter 等 Domain 拦截服务使用规则3（规则3与规则1拦截域名一致，仅格式差异）\n")
-            f.write("4. 规则1’、2’、3’为规则1、2、3的 Lite 版，仅针对国内域名拦截，体积较小\n\n")
-            f.write("| 规则 | 原始链接 | 加速链接 | 适配说明 |\n")
-            f.write("|:-|:-|:-|:-|\n")
-            f.write("| 规则1：DNS 拦截 | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt) | AdGuard、AdGuard Home 等 |\n")
-            f.write("| 规则1'：DNS 拦截 Lite | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdnslite.txt) | AdGuard、AdGuard Home 等 |\n")
-            f.write("| 规则2：插件拦截 | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt) | AdGuard 等 |\n")
-            f.write("| 规则2'：插件拦截 Lite | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilterslite.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilterslite.txt) | AdGuard 等 |\n")
-            f.write("| 规则3：Domain 拦截 | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdomain.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdomain.txt) | InviZible Pro、personalDNSfilter 等 |\n")
-            f.write("| 规则3'：Domain 拦截 Lite | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdomainlite.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdomainlite.txt) | InviZible Pro、personalDNSfilter 等 |\n")
-
+            f.write("4. 规则1’、2’、3’为规则1、2、3的 Lite 版，仅针对国内域名拦截，体积较小\n")
+            tmp = "| 规则 | 原始链接 |"
+            for i in range(1, len(self.proxyList)):
+                tmp += " 加速链接%d |"%(i)
+            tmp += " 适配说明 |\n"
+            f.write(tmp)
+            tmp = "|" + ":-|" * ( 1 + len(self.proxyList) + 1) + "\n"
+            f.write(tmp)
+            f.write("| 规则1：DNS 拦截 |" + self.__subscribeLink("adblockdns.txt") + " AdGuard、AdGuard Home 等 |\n")
+            f.write("| 规则1'：DNS 拦截 Lite |" + self.__subscribeLink("adblockdnslite.txt") + " AdGuard、AdGuard Home 等 |\n")
+            f.write("| 规则2：插件拦截 |" + self.__subscribeLink("adblockfilters.txt") + " AdGuard 等 |\n")
+            f.write("| 规则2'：插件拦截 Lite |" + self.__subscribeLink("adblockfilterslite.txt") + " AdGuard 等 |\n")
+            f.write("| 规则3：Domain 拦截 |" + self.__subscribeLink("adblockdomain.txt") + " InviZible Pro、personalDNSfilter 等 |\n")
+            f.write("| 规则3'：Domain 拦截 Lite |" + self.__subscribeLink("adblockdomainlite.txt") + " InviZible Pro、personalDNSfilter 等 |\n")
             f.write("\n")
+
             f.write("## 上游规则源\n")
             f.write("1. 感谢各位广告过滤规则维护大佬们的辛苦付出。\n")
             f.write("2. 不再引用[anti-AD](https://anti-ad.net/adguard.txt)、[yhosts](https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts.txt)，具体原因见[Mosney/anti-anti-AD](https://github.com/Mosney/anti-anti-AD)。\n")
@@ -83,10 +111,17 @@ class ReadMe(object):
             f.write("4. 移除[ADgk](https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt)，项目超过 1 年未更新。\n")
             f.write("5. 不再引用[NEO DEV HOST](https://github.com/neodevpro/neodevhost/blob/master/lite_adblocker)，原因见[Issues 85](https://github.com/217heidai/adblockfilters/issues/85)。\n")
             f.write("\n")
-            f.write("| 规则 | 类型 | 原始链接 | 加速链接 | 更新日期 |\n")
-            f.write("|:-|:-|:-|:-|:-|\n")
+
+            tmp = "| 规则 | 类型 | 原始链接 |"
+            for i in range(1, len(self.proxyList)):
+                tmp += " 加速链接%d |"%(i)
+            tmp += " 更新日期 |\n"
+            f.write(tmp)
+            tmp = "|" + ":-|" * ( 2 + len(self.proxyList) + 1) + "\n"
+            f.write(tmp)
             for rule in self.ruleList:
-                f.write("| %s | %s | [原始链接](%s) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/%s) | %s |\n" % (rule.name,rule.type,rule.url,rule.filename,rule.latest))
+                f.write("| %s | %s |%s %s |\n" % (rule.name, rule.type, self.__subscribeLink(rule.filename, rule.url),rule.latest))
             f.write("\n")
+            
             f.write("## Star History\n")
             f.write("[![Star History Chart](https://api.star-history.com/svg?repos=217heidai/adblockfilters&type=Date)](https://star-history.com/#217heidai/adblockfilters&Date)\n")
---
+++ refreshcdn.py
@@ -0,0 +1,59 @@
+import os
+import asyncio
+from typing import List,Tuple
+
+import httpx
+from loguru import logger
+
+class RefreshCDN(object):
+    def __init__(self):
+        self.pwd = os.getcwd() + '/rules'
+        self.blockList = [
+            "apple-cn.txt",
+            "black.txt",
+            "china.txt",
+            "CN-ip-cidr.txt",
+            "direct-list.txt",
+            "domain.txt",
+            "google-cn.txt",
+            "white.txt"
+        ]
+
+    def __getRuleList(self, pwd:str) -> List[str]:
+        L = []
+        cmd = 'cd %s && ls | grep .txt' %(pwd)
+        process = os.popen(cmd)
+        output = process.read()
+        process.close()
+        result = output.split("\n")
+        for fileName in result:
+            if len(fileName) >0 and fileName not in self.blockList:
+                L.append(fileName)
+        return L
+
+    async def __refresh(self, fileName):
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get("https://purge.jsdelivr.net/gh/217heidai/adblockfilters@main/rules/%s"%(fileName))
+                response.raise_for_status()
+                status = response.json().get("status", "")
+                logger.info(f'%s refresh status: %s' % (fileName, status))
+        except Exception as e:
+            logger.error(f'%s refresh failed: %s' % (fileName, e))
+
+    def refresh(self):
+        ruleList = self.__getRuleList(self.pwd)
+        # 启动异步循环
+        loop = asyncio.get_event_loop()
+        # 添加异步任务
+        taskList = []
+        for rule in ruleList:
+            logger.info("refresh %s..."%(rule))
+            task = asyncio.ensure_future(self.__refresh(rule))
+            taskList.append(task)
+        # 等待异步任务结束
+        loop.run_until_complete(asyncio.wait(taskList))
+
+if __name__ == '__main__':
+    cdn = RefreshCDN()
+    cdn.refresh()
