9fdc6de5c28008ca2ca9a19ede0ea0bf5d1d9466
217heidai <217heidai@gmail.com>
2025-09-28 09:17:45 +0800

    fix readme

--- readme.py
+++ readme.py
@@ -121,8 +121,8 @@ class ReadMe(object):
             f.write("| 规则11' |" + self.__subscribeLink("adblocksingboxlite.json") + " sing-box 1.12.x json |\n")
             f.write("| 规则12 |" + self.__subscribeLink("adblocksingbox.srs") + " sing-box 1.12.x srs |\n")
             f.write("| 规则12' |" + self.__subscribeLink("adblocksingboxlite.srs") + " sing-box 1.12.x srs |\n")
-            f.write("| 规则13 |" + self.__subscribeLink("adblockloon.list") + " Loon、Sugar |\n")
-            f.write("| 规则13' |" + self.__subscribeLink("adblockloonlite.list") + " Loon、Sugar |\n")
+            f.write("| 规则13 |" + self.__subscribeLink("adblockloon.list") + " Loon、Surge |\n")
+            f.write("| 规则13' |" + self.__subscribeLink("adblockloonlite.list") + " Loon、Surge |\n")
             f.write("\n")
 
             f.write("## 上游规则源\n")
